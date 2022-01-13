04bdc032ded98a252d22d77361e404a0616a7c71
kang su hyun <kangsu02@gmail.com>
2022-01-13 10:15:48 +0900

    initial commit

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,37 +1,18 @@
 module.exports = {
   root: true,
   env: {
-      browser: true
+    node: true
   },
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended'
+  ],
   parserOptions: {
-      parser: 'babel-eslint',
-      sourceType: 'module'
+    ecmaVersion: 2020
   },
-  extends: [
-      'plugin:vue/vue3-essential',
-      'eslint:recommended'
-  ],
-  plugins: [
-      'import'
-  ],
   rules: {
-      'class-methods-use-this': 'off',
-      'generator-star-spacing': 'off',
-      'arrow-parens': 'off',
-      'one-var': 'off',
-      'prefer-promise-reject-errors': 'off',
-      'quote-props': 'off',
-      'prefer-const': 'off',
-      'quotes': ['error', 'single'],
-      'no-multiple-template-root': 'off',
-      'import/first': 'off',
-      'import/named': 'error',
-      'import/namespace': 'error',
-      'import/default': 'error',
-      'import/export': 'error',
-      'import/extensions': 'off',
-      'import/no-unresolved': 'off',
-      'import/no-extraneous-dependencies': 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
