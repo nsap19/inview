b44cb5e43b60b72f31eed30622b3fa9b4d94f782
손수연 <sksy368@naver.com>
2022-01-19 04:10:55 +0900

    Feat: 회원탈퇴, 로그인, 회원가입 기능
    회원탈퇴
    로그인 (jwt 토큰 처리 제외)
    회원가입 (이메일 인증 제외)
    jwt 토큰 처리 환경 세팅
    
    #Resolves: #44, #45, #61, #87
    #Related to: #71, #54

--- backend/src/main/java/com/ssafy/api/service/UserServiceImpl.java
+++ backend/src/main/java/com/ssafy/api/service/UserServiceImpl.java
@@ -24,18 +24,39 @@ public class UserServiceImpl implements UserService {
 	PasswordEncoder passwordEncoder;
 	
 	@Override
-	public User createUser(UserRegisterPostReq userRegisterInfo) {
+	public String createUser(UserRegisterPostReq userRegisterInfo) {
 		User user = new User();
-		user.setUserId(userRegisterInfo.getId());
+		user.setEmail(userRegisterInfo.getEmail());
+		user.setNickname(userRegisterInfo.getNickname());
 		// 보안을 위해서 유저 패스워드 암호화 하여 디비에 저장.
 		user.setPassword(passwordEncoder.encode(userRegisterInfo.getPassword()));
-		return userRepository.save(user);
+		if(getUserByEmail(user.getEmail()) == null) { // db에 유저 조회 -> 이메일 중복 검사
+			if(getUserByNickname(user.getNickname()) == null) {
+				userRepository.save(user);
+				return "회원가입 성공";
+			}
+			else
+				return "이미 등록된 닉네임입니다.";
+		}
+		else
+			return "이미 등록된 이메일입니다.";
 	}
 
 	@Override
-	public User getUserByUserId(String userId) {
-		// 디비에 유저 정보 조회 (userId 를 통한 조회).
-		User user = userRepositorySupport.findUserByUserId(userId).get();
+	public User getUserByEmail(String email) {
+		// db에 유저 정보 조회 (email 를 통한 조회).
+		User user = userRepositorySupport.findUserByEmail(email).get();
 		return user;
+	}
+	
+	@Override
+	public User getUserByNickname(String nickname) {
+		User user = userRepositorySupport.findUserByNickname(nickname).get();
+		return user;
+	}
+
+	@Override
+	public void deleteUser(int user_id) {
+		userRepositorySupport.deleteByUserId(user_id);
 	}
 }
--- backend/src/main/java/com/ssafy/db/entity/User.java
+++ backend/src/main/java/com/ssafy/db/entity/User.java
@@ -6,7 +6,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  * 유저 모델 정의.
@@ -14,13 +18,20 @@ import javax.persistence.Entity;
 @Entity
 @Getter
 @Setter
-public class User extends BaseEntity{
-    String position;
-    String department;
-    String name;
-    String userId;
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    int user_id;
+    
+    @Column(length = 100, nullable = false, unique = true)
+    String email;
+    
+    @Column(length = 45, nullable = false, unique = true)
+    String nickname;
 
     @JsonIgnore
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(length = 100, nullable = false)
     String password;
 }
