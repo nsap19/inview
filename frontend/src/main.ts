072e4922518fff239c72ede4704361099f48924b
kang su hyun <kangsu02@gmail.com>
2022-02-02 18:48:37 +0900

    Feat: 연결 회의 생성 API
    회의 생성 폼 종료 시간 추가
    회의 생성 폼 시작 시간, 종료 시간 유효성 검사 추가
    회의 생성 요청 함수 생성
    
    Related to: #138

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,6 +8,8 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import "@/assets/common.css"
 import ko from 'element-plus/es/locale/lang/ko'
+import axios from 'axios'
 
+axios.defaults.baseURL = process.env.VUE_APP_API_URL
 
 createApp(App).use(store).use(router).use(ElementPlus, {locale: ko}).mount('#app')
--- frontend/src/views/CreateMeeting.vue
+++ frontend/src/views/CreateMeeting.vue
@@ -13,39 +13,43 @@
         <el-input v-model="ruleForm.title" placeholder="제목을 입력해주세요"></el-input>
       </el-form-item>
       <el-form-item label="직군" prop="industry">
-        <el-select v-model="ruleForm.industry" placeholder="직군을 선택해주세요" class="w-100">
-          <el-option label="Zone one" value="shanghai"></el-option>
-          <el-option label="Zone two" value="beijing"></el-option>
-        </el-select>
+        <IndustrySearchBar v-model="ruleForm.industry" />
       </el-form-item>
       <el-form-item label="회사">
-        <CompanySearchBar/>
+        <CompanySearchBar v-model="ruleForm.company" />
       </el-form-item>
       <el-form-item label="참가 인원">
-        <el-select v-model="ruleForm.userLimit" placeholder="최대 참가 인원을 선택해주세요" class="w-100">
-          <el-option label="Zone one" value="shanghai"></el-option>
-          <el-option label="Zone two" value="beijing"></el-option>
-        </el-select>
+        <el-radio-group v-model="ruleForm.userLimit" size="large">
+          <el-radio-button label="1"></el-radio-button>
+          <el-radio-button label="2"></el-radio-button>
+          <el-radio-button label="3"></el-radio-button>
+          <el-radio-button label="4"></el-radio-button>
+          <el-radio-button label="5"></el-radio-button>
+          <el-radio-button label="6"></el-radio-button>
+        </el-radio-group>
       </el-form-item>
-      <el-form-item label="시작 시간">
+      <el-form-item label="시작 시간" prop="startTime">
         <el-col :span="11">
           <el-date-picker
-            v-model="ruleForm.date1"
-            type="date"
+            v-model="ruleForm.startTime"
+            type="datetime"
+            format="YYYY-MM-DD HH:mm"
+            value-format="YYYY-MM-DD HH:mm"
             placeholder="날짜"
             style="width: 100%"
           ></el-date-picker>
         </el-col>
-        <el-col :span="2" class="text-center">
-          <span class="text-gray-500">-</span>
-        </el-col>
+      </el-form-item>
+      <el-form-item label="종료 시간" prop="endTime">
         <el-col :span="11">
-          <el-time-picker
-            v-model="ruleForm.date2"
-            placeholder="시간"
-            format="HH:mm"
+          <el-date-picker
+            v-model="ruleForm.endTime"
+            type="datetime"
+            format="YYYY-MM-DD HH:mm"
+            value-format="YYYY-MM-DD HH:mm"
+            placeholder="날짜"
             style="width: 100%"
-          ></el-time-picker>
+          ></el-date-picker>
         </el-col>
       </el-form-item>
       <el-form-item label="비밀번호">
@@ -65,40 +69,63 @@
 
 <script lang="ts">
 import { defineComponent, reactive, ref } from 'vue'
-import CompanySearchBar from '@/components/SearchFilterBar/CompanySearchBar.vue';
+import CompanySearchBar from '@/components/SearchFilterBar/CompanySearchBar.vue'
+import IndustrySearchBar from '@/components/SearchFilterBar/IndustrySearchBar.vue'
 import type { ElForm } from 'element-plus'
+import axios from 'axios'
 
 export default defineComponent({
   name: 'CreateMeeting',
   components: {
-    CompanySearchBar,
+    CompanySearchBar, IndustrySearchBar
   },
   setup() {
     const ruleFormRef = ref<InstanceType<typeof ElForm>>()
-    const submitForm = (formEl: InstanceType<typeof ElForm> | undefined) => {
-      if (!formEl) return
-      formEl.validate((valid) => {
-        if (valid) {
-          console.log('submit!')
-        } else {
-          console.log('error submit!')
-          return false
-        }
-      })
-    }
+
     const ruleForm = reactive({
       title: '',
       industry: '',
       company: '',
       userLimit: '',
-      date1: '',
-      date2: '',
+      startTime: '',
+      endTime: '',
       password: ''
     })
 
-    const onSubmit = () => {
-      console.log('submit!')
+    // eslint-disable-next-line
+    const validStartTime = (rule: any, value: any, callback: any) => {
+      const datetime_object = new Date()
+      const current_datetime = datetime_object.getFullYear() + "-"
+                              + ("0" + (1 + datetime_object.getMonth())).slice(-2) + "-" 
+                              + ("0" + datetime_object.getDate()).slice(-2) + " " 
+                              + datetime_object.getHours() + ":" + datetime_object.getUTCMinutes()
+      if (ruleForm.startTime === '') {
+        callback()
+      } else if (ruleForm.startTime < current_datetime) {
+        callback(new Error('시작 시간은 현재 시간 이후로 설정해주세요'))
+      } else {
+        callback()
+      }
     }
+
+    // eslint-disable-next-line
+    const validEndTime = (rule: any, value: any, callback: any) => {
+      const datetime_object = new Date()
+      const current_datetime = datetime_object.getFullYear() + "-"
+                              + ("0" + (1 + datetime_object.getMonth())).slice(-2) + "-" 
+                              + ("0" + datetime_object.getDate()).slice(-2) + " " 
+                              + datetime_object.getHours() + ":" + datetime_object.getUTCMinutes()
+      if (ruleForm.endTime === '') {
+        callback()
+      } else if (ruleForm.startTime && ruleForm.endTime < ruleForm.startTime) {
+        callback(new Error('종료 시간은 시작 시간 이후로 설정해주세요'))
+      } else if (ruleForm.endTime < current_datetime) {
+        callback(new Error('종료 시간은 현재 시간 이후로 설정해주세요'))
+      } else {
+        callback()
+      }
+    }
+
     const rules = reactive({
       title: [
         {
@@ -108,8 +135,8 @@ export default defineComponent({
         },
         {
           min: 1,
-          max: 20,
-          message: 'Length should be 1 to 20',
+          max: 100,
+          message: '제목은 1자 이상 100자 이하 입력해주세요',
           trigger: 'blur',
         },
       ],
@@ -120,25 +147,38 @@ export default defineComponent({
           trigger: 'blur',
         },
       ],
-      date1: [
-        {
-          type: 'date',
-          required: true,
-          message: 'Please pick a date',
-          trigger: 'change',
-        },
+      startTime: [
+        { 
+          validator: validStartTime,
+          trigger: "blur",
+        }
       ],
-      date2: [
-        {
-          type: 'date',
-          required: true,
-          message: 'Please pick a time',
-          trigger: 'change',
-        },
+      endTime: [
+        { 
+          validator: validEndTime,
+          trigger: "blur",
+        }
       ],
     })
 
-    return { ruleFormRef, ruleForm, rules, onSubmit, submitForm }
+    const submitForm = (formEl: InstanceType<typeof ElForm> | undefined) => {
+      if (!formEl) return
+      formEl.validate((valid) => {
+        if (valid) {
+          // console.log('submit!')
+          axios.post('/meeting', ruleForm).then(res => {
+            console.log(res.data)
+          }).catch(err => {
+            console.log(err.message)
+          })
+        } else {
+          // console.log('error submit!')
+          return false
+        }
+      })
+    }
+
+    return { ruleFormRef, ruleForm, rules, submitForm, validStartTime }
   }
 })
 </script>
