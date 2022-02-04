85ecda91af643378da6d95fd57eabfbaef71d663
손수연 <sksy368@naver.com>
2022-02-04 17:10:04 +0900

    Fix: 변경 로그인 기능
    로그인 시 데이터로 아이디와 이메일을 넘겨주도록 변경
    
    Resolves: #22

--- backend/src/main/java/com/ssafy/common/model/response/TokenResponseBody.java
+++ backend/src/main/java/com/ssafy/common/model/response/TokenResponseBody.java
@@ -13,6 +13,7 @@ import lombok.Setter;
 @ApiModel("TokenResponseBody")
 public class TokenResponseBody extends BaseResponseBody {
 	String token = null;
+	int userId = 0;
 	String nickname = null;
 	
 	public TokenResponseBody(Integer statusCode, String message, String token, String nickname) {
@@ -22,11 +23,12 @@ public class TokenResponseBody extends BaseResponseBody {
 		this.nickname = nickname;
 	}
 	
-	public static TokenResponseBody of(Integer statusCode, String message, String token, String nickname) {
+	public static TokenResponseBody of(Integer statusCode, String message, int userId, String nickname, String token) {
 		TokenResponseBody body = new TokenResponseBody();
 		body.message = message;
 		body.statusCode = statusCode;
 		body.token = token;
+		body.userId = userId;
 		body.nickname = nickname;
 		return body;
 	}
