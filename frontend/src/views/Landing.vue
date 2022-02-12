f20ca31d1189a6ccec4a35c37fc63e468570683e
o3ohanas <o3ohanas@gmail.com>
2022-02-13 01:54:55 +0900

    Feat: landing page
    사이트 소개를 위해 스크롤에 따라 내용이 변화는 landing page 제작

---
+++ frontend/src/views/Landing.vue
@@ -0,0 +1,683 @@
+<template>
+  <div id="app">
+    <main class="sticky-container" ref="sticky-container">
+      <div class="sticky">
+        <div class="slide-container">
+          <div class="slide" ref="sl0">
+            <div class="row">
+              <div class="col-md-6 mb-4">
+                <div class="slide-big-text">
+                  <br />
+                  <br />
+                  <br />
+                  <p>Inview :</p>
+                  <p>모두를 위한 면접 공간</p>
+                </div>
+                <div class="slide-small-text">
+                  <p>Inview를 통해 서로를 만나보세요.</p>
+                  <br />
+                  <a href="#" class="button1">Try it free</a>
+                  <br />
+                </div>
+              </div>
+              <div class="col-md-6 mb-4">
+                <img class="img-fluid" src="./../assets/video-conference.jpg" />
+              </div>
+            </div>
+          </div>
+          <div class="slide" ref="sl1">
+            <div class="row">
+              <div class="col-md-6 mb-4">
+                <div class="slide-big-text">
+                  <p></p>
+                  <p></p>
+                  <p>실시간 모의 면접</p>
+                </div>
+                <div class="slide-small-text">
+                  <p>
+                    직군, 회사 별로 개설된 면접을 검색하고 참여할 수 있습니다.
+                  </p>
+                  <p>같은 목표를 가진 사람들끼리</p>
+                  <p>효율적으로 면접 스터디가 가능합니다.</p>
+                  <br />
+                  <a href="#" class="button2">Try it free</a>
+                  <br />
+                </div>
+              </div>
+              <div class="col-md-6 mb-4">
+                <div
+                  class="card card-cover h-100 overflow-hidden text-white rounded-5 shadow-lg"
+                  style="background-color: #f3f0d7"
+                >
+                  <div
+                    class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1"
+                  >
+                    <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
+                      Short title, long jacket
+                    </h2>
+                    <ul class="d-flex list-unstyled mt-auto"></ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="scdown" ref="scdown">
+            <div class="scdown-text">아래로 스크롤하세요.</div>
+          </div>
+          <div class="slide" ref="sl2">
+            <div class="row">
+              <div class="col-md-6 mb-4">
+                <div class="slide-big-text">
+                  <p></p>
+                  <p></p>
+                  <p>추가적인 SNS와 Note는 No!</p>
+                </div>
+                <div class="slide-small-text">
+                  <p>면접에 집중할 수 있도록 Inview 내에서</p>
+                  <p>그룹 채팅, 파일 공유, 개인 메모장 기능을 제공합니다.</p>
+                  <p>면접 종료 후 파일로 다운받을 수 있습니다.</p>
+                  <br />
+                  <a href="#" class="button1">Try it free</a>
+                  <br />
+                </div>
+              </div>
+              <div class="col-md-6 mb-4">
+                <div
+                  class="card card-cover h-100 overflow-hidden text-white rounded-5 shadow-lg"
+                  style="background-color: #cee5d0"
+                >
+                  <div
+                    class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1"
+                  >
+                    <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
+                      Short title, long jacket
+                    </h2>
+                    <ul class="d-flex list-unstyled mt-auto"></ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="slide sl3" ref="sl3">
+            <div class="row">
+              <div class="col-md-6 mb-4">
+                <div class="slide-big-text">
+                  <p></p>
+                  <p></p>
+                  <p>평가 시트 제공</p>
+                </div>
+                <div class="slide-small-text">
+                  <p>스터디엔 결과 보고서가 있어야 겠죠?</p>
+                  <p>Inview가 평가 시트를 제공합니다.</p>
+                  <p>그룹원끼리 평가 시트를 통해 피드백 제공이 가능합니다.</p>
+                  <p style="color:#ff7878;">* 평가시트는 면접 종료 후 html 파일로 제공됩니다.</p>
+                  <br />
+                  <a href="#" class="button3">Try it free</a>
+                  <br />
+                </div>
+              </div>
+              <div class="col-md-6 mb-4">
+                <div
+                  class="card card-cover h-100 overflow-hidden text-white rounded-5 shadow-lg"
+                  style="background-color: #e0c097"
+                >
+                  <div
+                    class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1"
+                  >
+                    <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
+                      Short title, long jacket
+                    </h2>
+                    <ul class="d-flex list-unstyled mt-auto"></ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="slide slide-left sl4"
+            ref="sl4"
+          >
+            <div class="sl4-content">
+              <div class="slide-big-text" ref="sl4-big">
+                <p ref="sl4-big-1">자세한</p>
+                <p ref="sl4-big-2">설명이</p>
+                <p ref="sl4-big-3">있을 수도 있어요</p>
+              </div>
+              <div class="slide-small-text">
+                <p>실제 동작 사진을 넣는건</p>
+                <p>어떠신가요??</p>
+                <p>ㅜㅜ</p>
+              </div>
+            </div>
+          </div>
+          <div class="wave" ref="wave">
+            <img class="img-fluid" src="./../assets/background.png" />
+          </div>
+
+          <div
+            class="slide slide-left sl5"
+            ref="sl5"
+            style="background-color: white"
+          >
+            <div class="container md-6 mb-4 center"></div>
+            <div class="slide-big-text">
+              <p>Inview가</p>
+              <p>당신을 응원합니다.</p>
+            </div>
+            <div class="slide-small-text">
+              <p>당신의 꿈을 Inview와 함께 준비하세요.</p>
+              <br />
+              <br />
+              <a href="#" class="button1">Try it free</a>
+              <br />
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import bezierEasing from "bezier-easing";
+
+const ease = bezierEasing(0.25, 0.1, 0.25, 1.0);
+const easeIn = bezierEasing(0.38, 0.01, 0.78, 0.13);
+const midSlow = bezierEasing(0, 0.7, 1, 0.3);
+
+const def = {
+  height: 7100,
+  elements: {
+    sl0: {
+      top: 0,
+      bottom: 500,
+      topStyle: {
+        opacity: 1,
+      },
+      bottomStyle: {
+        opacity: 0,
+      },
+    },
+    sl1: {
+      top: 500,
+      bottom: 1900,
+      topStyle: {
+        opacity: 0,
+        translateY: -60,
+      },
+      bottomStyle: {
+        opacity: 0,
+        translateY: 60,
+      },
+    },
+    scdown: {
+      top: 0,
+      bottom: 1000,
+      topStyle: {
+        opacity: 1,
+      },
+      bottomStyle: {
+        opacity: 0,
+      },
+    },
+    sl2: {
+      top: 1900,
+      bottom: 3200,
+      topStyle: {
+        opacity: 0,
+        translateY: -60,
+      },
+      bottomStyle: {
+        opacity: 0,
+        translateY: 60,
+      },
+    },
+    sl3: {
+      top: 3300,
+      bottom: 4600,
+      topStyle: {
+        opacity: 0,
+      },
+      bottomStyle: {
+        opacity: 0,
+      },
+    },
+    wave: {
+      top: 4500,
+      bottom: 5900,
+      topStyle: {
+        opacity: 0,
+        translateY: 300,
+      },
+      bottomStyle: {
+        opacity: 0,
+        translateY: 0,
+      },
+    },
+    sl4: {
+      top: 4700,
+      bottom: 6000,
+      topStyle: {
+        opacity: 0,
+      },
+      bottomStyle: {
+        opacity: 0,
+      },
+    },
+    sl5: {
+      top: 6100,
+      bottom: 9000,
+      topStyle: {
+        opacity: 0,
+      },
+      bottomStyle: {
+        opacity: 0,
+      },
+    },
+  },
+  animations: {
+    sl0: [
+      {
+        top: 0,
+        bottom: 500,
+        easing: midSlow,
+        styles: {
+          translateY: {
+            topValue: -300,
+            bottomValue: -60,
+          },
+          opacity: {
+            topValue: 0.5,
+            bottomValue: 1,
+          },
+        },
+      },
+      {
+        top: 300,
+        bottom: 500,
+        easing: easeIn,
+        styles: {
+          opacity: {
+            topValue: 1,
+            bottomValue: 0,
+          },
+        },
+      },
+    ],
+    sl1: [
+      {
+        top: 500,
+        bottom: 1900,
+        easing: midSlow,
+        styles: {
+          translateY: {
+            topValue: 60,
+            bottomValue: -60,
+          },
+        },
+      },
+      {
+        top: 500,
+        bottom: 800,
+        easing: ease,
+        styles: {
+          opacity: {
+            topValue: 0,
+            bottomValue: 1,
+          },
+        },
+      },
+      {
+        top: 1400,
+        bottom: 1900,
+        easing: easeIn,
+        styles: {
+          opacity: {
+            topValue: 1,
+            bottomValue: 0,
+          },
+        },
+      },
+    ],
+    scdown: [
+      {
+        top: 600,
+        bottom: 1000,
+        easing: easeIn,
+        styles: {
+          opacity: {
+            topValue: 1,
+            bottomValue: 0,
+          },
+        },
+      },
+    ],
+    sl2: [
+      {
+        top: 1900,
+        bottom: 3200,
+        easing: midSlow,
+        styles: {
+          translateY: {
+            topValue: 60,
+            bottomValue: -60,
+          },
+        },
+      },
+      {
+        top: 1900,
+        bottom: 2500,
+        easing: ease,
+        styles: {
+          opacity: {
+            topValue: 0,
+            bottomValue: 1,
+          },
+        },
+      },
+      {
+        top: 2600,
+        bottom: 3200,
+        easing: easeIn,
+        styles: {
+          opacity: {
+            topValue: 1,
+            bottomValue: 0,
+          },
+        },
+      },
+    ],
+    sl3: [
+      {
+        top: 3300,
+        bottom: 4600,
+        easing: midSlow,
+        styles: {
+          translateY: {
+            topValue: 60,
+            bottomValue: -60,
+          },
+        },
+      },
+      {
+        top: 3300,
+        bottom: 3900,
+        easing: ease,
+        styles: {
+          opacity: {
+            topValue: 0,
+            bottomValue: 1,
+          },
+        },
+      },
+      {
+        top: 4000,
+        bottom: 4600,
+        easing: easeIn,
+        styles: {
+          opacity: {
+            topValue: 1,
+            bottomValue: 0,
+          },
+        },
+      },
+    ],
+    wave: [
+      {
+        top: 4500,
+        bottom: 5300,
+        easing: ease,
+        styles: {
+          translateY: {
+            topValue: 200,
+            bottomValue: 0,
+          },
+          opacity: {
+            topValue: 0,
+            bottomValue: 0.4,
+          },
+        },
+      },
+      {
+        top: 5300,
+        bottom: 5900,
+        easing: easeIn,
+        styles: {
+          opacity: {
+            topValue: 0.4,
+            bottomValue: 0,
+          },
+        },
+      },
+    ],
+    sl4: [
+      {
+        top: 4700,
+        bottom: 6000,
+        easing: midSlow,
+        styles: {
+          translateY: {
+            topValue: 60,
+            bottomValue: -60,
+          },
+        },
+      },
+      {
+        top: 4700,
+        bottom: 5300,
+        easing: ease,
+        styles: {
+          opacity: {
+            topValue: 0,
+            bottomValue: 1,
+          },
+        },
+      },
+      {
+        top: 5400,
+        bottom: 6000,
+        easing: easeIn,
+        styles: {
+          opacity: {
+            topValue: 1,
+            bottomValue: 0,
+          },
+        },
+      },
+    ],
+    sl5: [
+      {
+        top: 6100,
+        bottom: 7100,
+        easing: midSlow,
+        styles: {
+          translateY: {
+            topValue: 60,
+            bottomValue: -60,
+          },
+        },
+      },
+      {
+        top: 6100,
+        bottom: 6700,
+        easing: ease,
+        styles: {
+          opacity: {
+            topValue: 0,
+            bottomValue: 1,
+          },
+        },
+      },
+    ],
+  },
+};
+
+let enabled = new Map();
+let disabled = new Map();
+
+const isAmong = (num, top, bottom) => num >= top && num <= bottom;
+
+const applyStyle = (element, styleName, value, unit = "px") => {
+  if (styleName === "translateY") {
+    // eslint-disable-next-line no-param-reassign
+    element.style.transform = `translateY(${value}${unit})`;
+    return;
+  }
+  if (styleName === "translateX") {
+    // eslint-disable-next-line no-param-reassign
+    element.style.transform = `translateX(${value}${unit})`;
+    return;
+  }
+  // eslint-disable-next-line no-param-reassign
+  element.style[styleName] = value;
+};
+
+export default {
+  el: "#app",
+  data() {
+    return {
+      message: "Welcome to Vue!",
+    };
+  },
+  mounted() {
+    this.init();
+    window.addEventListener("scroll", this.onScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.onScroll);
+  },
+
+  methods: {
+    init() {
+      this.initAnimation();
+    },
+    // 애니메이션 초기화
+    initAnimation() {
+      // Sticky Conainer 의 높이를 설정함.
+      this.$refs["sticky-container"].style.height = `${def.height}px`;
+
+      // disabled, enabled 를 비움.
+      disabled.clear();
+      enabled.clear();
+
+      // 모든 요소를 disabled 에 넣음.
+      for (const refname of Object.keys(def.elements)) {
+        disabled.set(refname, def.elements[refname]);
+      }
+
+      // 각 애니메이션을 enabled == false 로 만듬.
+      for (const refname of Object.keys(def.animations)) {
+        for (const animation of def.animations[refname]) {
+          animation.enabled = false;
+        }
+      }
+
+      // 초기 스타일 적용
+      disabled.forEach((obj, refname) => {
+        Object.keys(obj.topStyle).forEach((styleName) => {
+          const pushValue = obj.topStyle[styleName];
+          this.$refs[refname].style[styleName] = pushValue;
+        });
+      });
+
+      // 이미 요소의 범위 및 애니메이션의 범위에 있는 것들을 렌더링하기 위해
+      // 임의로 스크롤 이벤트 핸들러를 한 번 실행시킴.
+      this.onScroll();
+    },
+    applyStyles(currentPos, refname, styles, r, unit = "px") {
+      for (const style of Object.keys(styles)) {
+        const { topValue, bottomValue } = styles[style];
+        const calc = (bottomValue - topValue) * r + topValue;
+        console.log("calc : " + calc);
+        applyStyle(this.$refs[refname], style, calc, unit);
+      }
+    },
+    applyAllAnimation(currentPos, refname) {
+      const animations = def.animations[refname];
+      if (!animations) return;
+      for (const animation of animations) {
+        const { top: a_top, bottom: a_bottom, easing, styles } = animation;
+        const isIn = isAmong(currentPos, a_top, a_bottom);
+        // 만약 애니메이션이 새롭게 들어갈 때 혹은 나갈때 enabled 설정
+        if (isIn) {
+          if (!animation.enabled) animation.enabled = true;
+        } else if (!isIn && animation.enabled) {
+          if (currentPos <= a_top) {
+            this.applyStyles(currentPos, refname, styles, 0);
+          } else if (currentPos >= a_bottom) {
+            this.applyStyles(currentPos, refname, styles, 1);
+          }
+          // eslint-disable-next-line no-param-reassign
+          animation.enabled = false;
+        }
+
+        // 애니메이션이 enabled 라면, 애니메이션 적용.
+        if (animation.enabled) {
+          const r = easing((currentPos - a_top) / (a_bottom - a_top));
+          // eslint-disable-next-line no-param-reassign
+          this.applyStyles(currentPos, refname, styles, r);
+        }
+      }
+    },
+    onScroll() {
+      // 현재 스크롤 위치 파악
+      const scrollTop = window.scrollY || window.pageYOffset;
+      const currentPos = scrollTop + window.innerHeight / 2;
+      console.log("currentPos : " + currentPos);
+      // disabled 순회하며 활성화할 요소 찾기.
+      disabled.forEach((obj, refname) => {
+        // 만약 칸에 있다면 해당 요소 활성화
+        if (isAmong(currentPos, obj.top, obj.bottom)) {
+          console.log(refname + " " + obj.top + " " + obj.bottom);
+          enabled.set(refname, obj);
+          this.$refs[refname].classList.remove("disabled");
+          this.$refs[refname].classList.add("enabled");
+          disabled.delete(refname);
+        }
+      });
+
+      // enabled 순회하면서 해제할 요소를 체크
+      enabled.forEach((obj, refname) => {
+        const { top, bottom, topStyle, bottomStyle } = obj;
+        // console.log(`${top}, ${bottom}, ${topStyle}, ${bottomStyle}`);
+        // 범위 밖에 있다면
+        if (!isAmong(currentPos, top, bottom)) {
+          console.log(currentPos + " " + top + " " + bottom);
+          // 위로 나갔다면 시작하는 스타일 적용
+          if (currentPos <= top) {
+            Object.keys(topStyle).forEach((styleName) => {
+              applyStyle(this.$refs[refname], styleName, topStyle[styleName]);
+            });
+          }
+          // 아래로 나갔다면 끝나는 스타일적용
+          else if (currentPos >= bottom) {
+            Object.keys(bottomStyle).forEach((styleName) => {
+              applyStyle(
+                this.$refs[refname],
+                styleName,
+                bottomStyle[styleName]
+              );
+              // this.$refs[refname].style[styleName] = bottomStyle[styleName];
+            });
+          }
+
+          // 리스트에서 삭제하고 disabled로 옮김.
+          disabled.set(refname, obj);
+          this.$refs[refname].classList.remove("enabled");
+          this.$refs[refname].classList.add("disabled");
+          enabled.delete(refname);
+        }
+        // enable 순회중, 범위 내부에 제대로 있다면 각 애니메이션 적용시키기.
+        else {
+          this.applyAllAnimation(currentPos, refname);
+          console.log(currentPos + " " + refname + " 애니메이션 적용중");
+        }
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "./../style/landing.scss";
+</style>
