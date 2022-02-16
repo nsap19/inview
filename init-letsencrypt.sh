da75a16280c5ff6b719d58701a5bf3e823e7bf90
JMine97 <52027965+JMine97@users.noreply.github.com>
2022-02-17 03:09:32 +0900

    .

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,11 +5,11 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(www.i6a201.p.ssafy.io) // 각자 등록한 도메인
+domains="www.i6a201.p.ssafy.io"
 rsa_key_size=4096
-data_path="./data/certbot"
-email="{email}" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+data_path="../certbot"
+email="chlwjdals98" # Adding a valid address is strongly recommended
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -67,7 +67,7 @@ esac
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
 docker-compose run --rm --entrypoint "\
-  certbot certonly -a webroot -v --debug-challenges -w /var/www/certbot \
+  certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
     $domain_args \
